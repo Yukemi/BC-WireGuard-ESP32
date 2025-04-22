2a3287c99264bb803d1eecb9e9326f6a74421ee1
Lukas <240403@vutbr.cz>
2025-04-22 19:01:32 +0200

    First Commit

---
+++ main/main.cpp
@@ -0,0 +1,21 @@
+#include "Arduino.h"
+
+extern "C" void app_main()
+{
+initArduino();
+// pinMode(4, OUTPUT);
+// digitalWrite(4, HIGH);
+// Do your own thing
+
+/* Setup */
+
+/* ===== */
+
+/* Loop */
+while (1)
+{
+
+}
+
+/* ==== */
+}
